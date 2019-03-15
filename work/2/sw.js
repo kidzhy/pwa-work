70048a8db7bbe97e0edef25624e00c3123eb9b40
zhy <zhyznj@sina.com>
2019-03-15 15:56:25 +0800

    增加内容

--- work/2/sw.js
+++ work/2/sw.js
@@ -1,3 +1,13 @@
 self.addEventListener("fetch",function(evt){
+  //打印所有页面发起请求的记录
   console.log("Fetch request for:",evt.request.url);
+
+  if(evt.request.url.includes("style.css")){
+    evt.respondWith(
+      new Response(
+        ".pwa{background:green}",
+        {headers:{"Content-type":"text/css"}}
+      )
+    )
+  }
 });
