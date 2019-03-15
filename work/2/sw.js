454ef710418d1d958758443f533240ec8a13c6a5
zhy <zhyznj@sina.com>
2019-03-15 16:30:21 +0800

    增加图片响应

--- work/2/sw.js
+++ work/2/sw.js
@@ -2,6 +2,7 @@ self.addEventListener("fetch",function(evt){
   //打印所有页面发起请求的记录
   console.log("Fetch request for:",evt.request.url);
 
+  //拦截style.css并修改内容
   if(evt.request.url.includes("style.css")){
     evt.respondWith(
       new Response(
