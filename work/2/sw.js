325f197efbe4a937e76e1628fe641d8c71dbebdd
zhy <zhyznj@sina.com>
2019-03-15 16:42:56 +0800

    屏蔽 功能

--- work/2/sw.js
+++ work/2/sw.js
@@ -2,23 +2,23 @@ self.addEventListener("fetch",function(evt){
   //打印所有页面发起请求的记录
   console.log("Fetch request for:",evt.request.url);
 
-  //拦截style.css并修改内容
-  if(evt.request.url.includes("style.css")){
-    evt.respondWith(
-      new Response(
-        ".pwa{background:green}",
-        {headers:{"Content-type":"text/css"}}
-      )
-    )
-  }
-  //拦截一张图片换成另一张
-  if(evt.request.url.includes("1.png")){
-    evt.respondWith(
-      fetch("img/2.png").catch(e=>{
-        console.log(e);
-      })
-    )
-  }
+  // //拦截style.css并修改内容
+  // if(evt.request.url.includes("style.css")){
+  //   evt.respondWith(
+  //     new Response(
+  //       ".pwa{background:green}",
+  //       {headers:{"Content-type":"text/css"}}
+  //     )
+  //   )
+  // }
+  // //拦截一张图片换成另一张
+  // if(evt.request.url.includes("1.png")){
+  //   evt.respondWith(
+  //     fetch("img/2.png").catch(e=>{
+  //       console.log(e);
+  //     })
+  //   )
+  // }
 
   evt.respondWith(
     fetch(evt.request).catch(()=>{
