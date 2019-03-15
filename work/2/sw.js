a2b50aead52f3faff39879734afba7f425a7e6df
zhy <zhyznj@sina.com>
2019-03-15 16:41:24 +0800

    增加功能

--- work/2/sw.js
+++ work/2/sw.js
@@ -19,4 +19,13 @@ self.addEventListener("fetch",function(evt){
       })
     )
   }
+
+  evt.respondWith(
+    fetch(evt.request).catch(()=>{
+      new Response(
+        "你看到了一个神奇的页面"
+      )
+    })
+  )
+
 });
