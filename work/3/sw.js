f35bc1318ba552a532f19fc932eb1c97588ea45e
zhy <zhyznj@sina.com>
2019-03-18 14:39:50 +0800

    离线后访问离线页面

--- work/3/sw.js
+++ work/3/sw.js
@@ -1,7 +1,15 @@
-self.addEventListener("install",function(evt){
+self.addEventListener("install", function(evt) {
   evt.waitUntil(
-    caches.open("v1").then(function(cache){
-      return cache.add("404.html")
+    caches.open("v1").then(function(cache) {
+      return cache.add("404.html");
     })
-  )
+  );
+});
+
+self.addEventListener("fetch", function(evt) {
+  evt.respondWith(
+    fetch(evt.request).catch(function() {
+      return caches.match("404.html");
+    })
+  );
 });
