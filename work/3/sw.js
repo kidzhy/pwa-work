47fc239c6ae9796b201dcb5a9b1157eaadded050
zhy <zhyznj@sina.com>
2019-03-18 14:43:47 +0800

    1

--- work/3/sw.js
+++ work/3/sw.js
@@ -1,6 +1,6 @@
 self.addEventListener("install", function(evt) {
   evt.waitUntil(
-    caches.open("v1").then(function(cache) {
+    caches.open("v2").then(function(cache) {
       return cache.add("404.html");
     })
   );
