982e2e0e7d0118e1f8331b742c4442c1db4f95b6
zhy <zhyznj@sina.com>
2019-03-20 14:24:59 +0800

    tijiao

--- work/4/sw.js
+++ work/4/sw.js
@@ -1,5 +1,18 @@
-self.addEventListener("install",function(){
+var CACHE_NAME = "v1";
+var CACHED_URLS = [
+  "index.html",
+  "404.html",
+  "img/1.png",
+  "css/style/css"
+];
+
+self.addEventListener("install",function(evt){
   console.log("安装");
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(CACHED_URLS);
+    })
+  )
 });
 
 self.addEventListener("activate",function(){
@@ -9,4 +22,15 @@ self.addEventListener("activate",function(){
 self.addEventListener("fetch",function(evt){
   console.log("请求响应中");
   console.log(evt.request.url);
+  evt.respondWith(
+    fetch(evt.request).catch(function(){
+      return caches.match(evt.request).then(function(response){
+        if(response){
+          return response
+        }else if(evt.request.headers.get("accept").includes("text/html")){
+          return caches.match("404.html")
+        }
+      })
+    })
+  )
 });
